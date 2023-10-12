f5216f6756797c3e7410cfba87cf9f9bee8179fe
Ganesh Encore <encore@Ganeshs-MacBook-Pro.local>
2023-10-12 12:24:08 -0400

    Displaying ingridents in HTML DOM and removed Console.log

--- Recepi-API/script/index.js
+++ Recepi-API/script/index.js
@@ -25,7 +25,7 @@ function searchRecipes() {
   getApiList(searchInput);
 }
 
-// Fetch API data
+// Fetch API data for search
 async function getApiList(searchInput) {
   const response = await fetch(apiLink);
   const data = await response.json();
@@ -47,9 +47,9 @@ function showResults(data) {
   const resultDiv = document.getElementById("results");
   resultDiv.innerHTML = "";
 
-  data.forEach((item, index) => {
+  data.forEach((item) => {
     resultDiv.innerHTML += `
-    <p><img src ="${item.thumbnail_url}" alt="${item.name}" width ="20%"</p>
+    <p><img src ="${item.thumbnail_url}" alt="${item.name}" width ="20%" /></p>
     <p>${item.name}</p>
     <button onClick="showDetails(${item.id})">More Information</button>
     `;
@@ -61,12 +61,23 @@ function showNoResults() {
   resultDiv.innerHTML = `<p>There are no results</p>`;
 }
 
+// Fetch data for recipe details
+
 async function showDetails(recipeDetails) {
   const response = await fetch(apiLink);
   const data = await response.json();
   const recipesDetailsList = data.results;
   const showThis = recipesDetailsList.find(({ id }) => id === recipeDetails);
-  console.log(showThis.name);
-  console.log(showThis.total_time_tier.display_tier);
-  console.log(showThis.instructions);
+
+  const lists = showThis.instructions;
+  let instructionList = "";
+  lists.forEach((list) => {
+    instructionList += `<p>${list.display_text}</p>`;
+
+    document.getElementById("ingredient").innerHTML = `
+  <p>${showThis.name}</p>
+  <p>${showThis.total_time_tier.display_tier}</p>
+  <p> ${instructionList}</p>
+  `;
+  });
 }

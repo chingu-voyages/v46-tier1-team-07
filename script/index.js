61e7e24a7c84848359506db5239542e3816224c9
Sarah Thomas <sbade92@gmail.com>
2023-11-10 17:18:40 -0800

    Finished bio page, light-dark mode

--- script/index.js
+++ script/index.js
@@ -1,3 +1,57 @@
+//Dark and Light Mode
+
+let darkModeIndex = true;
+const toggleIndex = document.querySelector('#toggle-index');
+const toggle = document.querySelector('.dark-mode-toggle');
+const body = document.querySelector('body');
+const headerText = document.querySelector('header h1');
+const headerPhoto = document.querySelector('header img');
+const footerIcon = document.querySelectorAll('footer i');
+const dropDownLink = document.querySelector('.dropdown-menu a');
+const searchText = document.querySelector('.search-area h3');
+const displayText = document.querySelector('.display-mode');
+const searchButton = document.getElementById('searchBtn');
+const inputMessage = document.getElementById('inputMsg');
+
+toggleIndex.addEventListener('click', colorModeActivateIndex);
+
+function colorModeActivateIndex(){
+  if(darkModeIndex){
+    darkModeIndex = false;
+    body.style.backgroundColor = '#F7E7D5';
+    body.style.color = '#733416';
+    headerText.style.color = '#733416';
+    searchText.style.color = '#331609';
+    searchButton.style.color = '#331609';
+    inputMessage.style.color = '#331609';
+    headerPhoto.src = "./img/logo-light.PNG";
+    footerIcon.forEach(icon => icon.style.color = '#733416'); 
+    dropDownLink.style.color = '#733416';
+    dropDownLink.style.border = '2px dotted #733416';
+    displayText.innerText = 'Dark Mode';
+    toggle.classList.remove('fa-toggle-off');
+    toggle.classList.add('fa-toggle-on');
+
+  } else {
+    darkModeIndex = true;
+    body.style.backgroundColor = '#733416';
+    body.style.color = '#F7E7D5';
+    headerText.style.color = '#F7E7D5';
+    searchText.style.color = '#F7E7D5';
+    searchButton.style.color = '#F7E7D5';
+    inputMessage.style.color = '#F7E7D5';
+    headerPhoto.src = "./img/logo-dark.PNG";
+    footerIcon.forEach(icon => icon.style.color = '#F7E7D5'); 
+    dropDownLink.style.color = '#F7E7D5';
+    dropDownLink.style.border = '2px dotted #F7E7D5';
+    displayText.innerText = 'Light Mode';
+    toggle.classList.add('fa-toggle-off');
+    toggle.classList.remove('fa-toggle-on');
+  }
+};
+
+
+
 const resultDiv = document.getElementById("results");
 let dataSaved = null;
 let searchResults = null;
